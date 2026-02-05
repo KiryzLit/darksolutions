09c8d144b46f196000f2256edc90c6b6dba65f40
moonrise <81956006+KiryzLit@users.noreply.github.com>
2026-02-05 14:59:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -47,6 +47,18 @@ function toggleLang(){
   checkVersion();
 }
 
+// Плавный переход по якорям
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', function(e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id)?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+
 loadLang();
 checkVersion();
 loadChangelog();
